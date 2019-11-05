561af9520b1fb747abf1d84082a471623c5b1e9e
Hariharan Venkitachalam <vharihar@in.ibm.com>
2019-11-05 15:51:02 +0530

    new app

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     
+app.use(express.static("public"));
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
