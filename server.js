c758449cc89c1ed1ec799e558dc2a537716554a1
invhariharan77 <invhariharan@yahoo.com>
2019-11-05 12:11:15 +0000

    fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
 
-// trying
+// trying..1
 app.use(express.static("public"));
 Object.assign=require('object-assign')
 
