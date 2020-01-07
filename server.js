6100011f5929af6e9de4cfd8fc8e25b4e3f010ff
invhariharan77 <invhariharan@yahoo.com>
2020-01-07 23:08:21 +0000

    Apply Defects

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-
-app.use(express.static("public"));
+    
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
