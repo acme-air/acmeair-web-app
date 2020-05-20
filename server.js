b957e93313493a4455b8565c020d035eda8a2b2b
Hariharan Venkitachalam <invhariharan@gmail.com>
2020-05-20 11:28:41 +0000

    Fix IP

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var todos = ['new'];
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : '172.30.114.168',
+    host     : '172.30.114.186',
     user     : 'demoadmin',
     password : 'Hello123!',
     database : 'sampledb',
